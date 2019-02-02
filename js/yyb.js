a48fb27e1e78adb3de9fba1009a7a4d2e1115511
YB198 <yyb1998@qq.com>
2019-02-02 11:57:33 +0800

    完善导航栏

--- js/yyb.js
+++ js/yyb.js
@@ -1,18 +1,58 @@
 var yyb = (function(){
-    var $s = $('.s-dropdown');
-    var $more = $('.dropdown-more');
+    var $s1 = $('.s1');
+    var $m1 = $('.m1');
+    var $s2 = $('.s2');
+    var $m2 = $('.m2');
+    var $s3 = $('.s3');
+    var $n3 = $('.n3');
+    var $s4 = $('.s4');
+    var $c4 = $('.c4');
+    var $s5 = $('.s5');
+    var $m5 = $('.m5');
 	return{
         init(){
             this.event();
         },
         event(){
-            $s.mouseenter(function(){
-                $s.addClass('hover');
-                $more.show();
+            $s1.mouseenter(function(){
+                $(this).addClass('hover');
+                $m1.show();
             })
-            $s.mouseleave(function(){
-                $s.removeClass('hover');
-                $more.hide();
+            $s1.mouseleave(function(){
+                $(this).removeClass('hover');
+                $m1.hide();
+            })
+            $s2.mouseenter(function(){
+                $(this).addClass('hover');
+                $m2.show();
+            })
+            $s2.mouseleave(function(){
+                $(this).removeClass('hover');
+                $m2.hide();
+            })
+            $s3.mouseenter(function(){
+                $(this).addClass('hover');
+                $n3.show();
+            })
+            $s3.mouseleave(function(){
+                $(this).removeClass('hover');
+                $n3.hide();
+            })
+            $s4.mouseenter(function(){
+                $(this).addClass('hover');
+                $c4.show();
+            })
+            $s4.mouseleave(function(){
+                $(this).removeClass('hover');
+                $c4.hide();
+            })
+            $s5.mouseenter(function(){
+                $(this).addClass('hover');
+                $m5.show();
+            })
+            $s5.mouseleave(function(){
+                $(this).removeClass('hover');
+                $m5.hide();
             })
         }
     }
